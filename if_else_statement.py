0f1ecbf027a51ff38e7c0d2536d03f7cb05cfa88
REL-TEK <bernardboateng584@gmail.com>
2024-10-08 15:41:23 -0700

    if_else_statement

---
+++ if_else_statement.py
@@ -0,0 +1,9 @@
+# using if else statement to set a password
+correct_password = "@_REL-TEK" 
+
+password = input("Enter your password: ")
+
+if password == correct_password:
+    print("Access Granted")
+else:
+    print("Access Denied")
